80b21852c93aa1b0b54b82281164a879f4f9b3f6
unknown <j.devin.vial@gmail.com>
2019-03-16 20:56:23 -0600

    testing

--- server.js
+++ server.js
@@ -2,27 +2,31 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
+var params = [];
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', 'views');
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/index'));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const { Pool } = require("pg");
-const connectionString = process.env.DATABASE_URL || "dbblma8kvlrik9";
+const connectionString = process.env.DATABASE_URL || "postgres://test@localhost:5432/postgres";
 const pool = new Pool({connectionString: connectionString});
 
 app.get('/manifest', getManifest);
 
-function getManifest(request, response) {
+app.get('/', function(req, res){
+  console.log("recieved a root request");
+  res.render("index");
+});
+
+function getManifest(req, res) {
   getDB(function(error, result) {
-    if (error || result == null || result.length != 1) {
-      response.status(500).json({success: false, data: error});
-    } else {
-      const name = result[1];
-      response.status(200).json(name);
-    }
+    params = JSON.stringify(result);
+    console.log("PARAMS: " + params);
+    console.log("testing " + result[2].name);
+    res.render("index", result);
   });
 }
 
@@ -31,9 +35,7 @@ function getDB(callback) {
 
   const sql = "SELECT * FROM manifest";
 
-  const params = [];
-
-  pool.query(sql, params, function(err, result) {
+  pool.query(sql, function(err, result) {
     if (err) {
       console.log("Error in query: ")
       console.log(err);
@@ -41,7 +43,6 @@ function getDB(callback) {
     }
 
     console.log("found result: " + JSON.stringify(result.rows));
-
     callback(null, result.rows);
   });
 }
