491bed712762ccf7833439545b5d0337c3e3a28d
unknown <j.devin.vial@gmail.com>
2019-03-16 22:35:29 -0600

    almost done

--- server.js
+++ server.js
@@ -7,26 +7,29 @@ var params = [];
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => res.render('pages/index'));
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
 
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL || "postgres://test@localhost:5432/postgres";
 const pool = new Pool({connectionString: connectionString});
 
-app.get('/manifest', getManifest);
-
-app.get('/', function(req, res){
-  console.log("recieved a root request");
-  res.render("index");
+app.get("/", function(req, res){
+  console.log("received a request for /");
+  res.render("index.ejs");
 });
+
+app.get('/manifest', getManifest);
 
 function getManifest(req, res) {
   getDB(function(error, result) {
-    params = JSON.stringify(result);
-    console.log("PARAMS: " + params);
-    console.log("testing " + result[2].name);
-    res.render("index", result);
+    //params = JSON.stringify(result);
+    //console.log("PARAMS: " + params);
+    var object = [];
+    for (var i = 0; i < result.length; i++) {
+      object[i] = result[i];
+    }
+    //console.log("testing " + object[0].name);
+    res.render("index", {object: object});
   });
 }
 
@@ -46,3 +49,4 @@ function getDB(callback) {
     callback(null, result.rows);
   });
 }
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
