fbfb9a703ca8158c2a278e8cb160bf604b16af68
unknown <j.devin.vial@gmail.com>
2019-03-23 16:39:46 -0600

    added post for DB INSERTS

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
-var params = [];
 
 // Set Up
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
 
 // DB Connection
 const { Pool } = require("pg");
@@ -21,11 +27,38 @@ app.get("/", function(req, res){
   res.render("index.ejs");
 });
 
+// DB Update Request
+app.post('/submit', updateManifest);
+
+// DB Update Request
+function updateManifest(req, res) {
+  // Log From Contents
+  console.log(req.body);
+  var DBname = req.body.name;
+  var DBstartloc = req.body.startloc;
+  var DBendloc = req.body.endloc;
+  // Create DB INSERT statement
+  const sql = "INSERT INTO manifest (name, startloc, endloc) VALUES ('"+DBname+"', '"+DBstartloc+"', '"+DBendloc+"')";
+  // INSERT INTO DB and log any errors
+  pool.query(sql, function(err, result) {
+    if (err) {
+      console.log("ERROR in INSERT: ")
+      console.log(err);
+    }
+  });
+  // display the homepage
+  res.redirect('/');
+}
+
+
+
+
 // /Manifest Request
 app.get('/manifest', getManifest);
 
-// 
+// Manifest Request
 function getManifest(req, res) {
+  console.log("testing")
   getDB(function(error, result) {
     // Send JSON to client
     res.json(result);
